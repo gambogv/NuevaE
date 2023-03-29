7b0e83906ccb3406f55962778258c224bdd4a4b1
GABRIEL ANGEL VELASQUEZ RINCON <gabrielvelasquez256153@correo.itm.edu.co>
2023-03-29 18:41:57 -0500

    Paginacion y filtos paisesCityState

--- Sales.API/Data/SeedDb.cs
+++ Sales.API/Data/SeedDb.cs
@@ -1,14 +1,19 @@
-﻿using Sales.Shared.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using Sales.API.Services;
+using Sales.Shared.Entities;
+using Sales.Shared.Responses;
 
 namespace Sales.API.Data
 {
     public class SeedDb
     {
         private readonly DataContext _context;
-
-        public SeedDb(DataContext context)
+        private readonly IApiService _apiService;
+        
+        public SeedDb(DataContext context, IApiService apiService)
         {
             _context = context;
+            _apiService = apiService;
         }
 
         public async Task SeedAsync()
@@ -21,161 +26,58 @@ namespace Sales.API.Data
         {
             if (!_context.Countries.Any())
             {
-                _context.Countries.Add(new Country
+                Response responseCountries = await _apiService.GetListAsync<CountryResponse>("/v1", "/countries");
+                if (responseCountries.IsSuccess)
                 {
-                    Name = "Colombia",
-                    States = new List<State>()
+                    List<CountryResponse> countries = (List<CountryResponse>)responseCountries.Result!;
+                    foreach (CountryResponse countryResponse in countries)
                     {
-                        new State()
+                        Country? country = await _context.Countries!.FirstOrDefaultAsync(c => c.Name == countryResponse.Name!)!;
+                        if (country == null)
                         {
-                            Name = "Antioquia",
-                            Cities = new List<City>() {
-                                new City() { Name = "Medellín" },
-                                new City() { Name = "Itagüí" },
-                                new City() { Name = "Envigado" },
-                                new City() { Name = "Bello" },
-                                new City() { Name = "Rionegro" },
+                            country = new() { Name = countryResponse.Name!, States = new List<State>() };
+                            Response responseStates = await _apiService.GetListAsync<StateResponse>("/v1", $"/countries/{countryResponse.Iso2}/states");
+                            if (responseStates.IsSuccess)
+                            {
+                                List<StateResponse> states = (List<StateResponse>)responseStates.Result!;
+                                foreach (StateResponse stateResponse in states!)
+                                {
+                                    State state = country.States!.FirstOrDefault(s => s.Name == stateResponse.Name!)!;
+                                    if (state == null)
+                                    {
+                                        state = new() { Name = stateResponse.Name!, Cities = new List<City>() };
+                                        Response responseCities = await _apiService.GetListAsync<CityResponse>("/v1", $"/countries/{countryResponse.Iso2}/states/{stateResponse.Iso2}/cities");
+                                        if (responseCities.IsSuccess)
+                                        {
+                                            List<CityResponse> cities = (List<CityResponse>)responseCities.Result!;
+                                            foreach (CityResponse cityResponse in cities)
+                                            {
+                                                if (cityResponse.Name == "Mosfellsbær" || cityResponse.Name == "Șăulița")
+                                                {
+                                                    continue;
+                                                }
+                                                City city = state.Cities!.FirstOrDefault(c => c.Name == cityResponse.Name!)!;
+                                                if (city == null)
+                                                {
+                                                    state.Cities.Add(new City() { Name = cityResponse.Name! });
+                                                }
+                                            }
+                                        }
+                                        if (state.CitiesNumber > 0)
+                                        {
+                                            country.States.Add(state);
+                                        }
+                                    }
+                                }
                             }
-                        },
-                        new State()
-                        {
-                            Name = "Bogotá",
-                            Cities = new List<City>() {
-                                new City() { Name = "Usaquen" },
-                                new City() { Name = "Champinero" },
-                                new City() { Name = "Santa fe" },
-                                new City() { Name = "Useme" },
-                                new City() { Name = "Bosa" },
+                            if (country.StatesNumber > 0)
+                            {
+                                _context.Countries.Add(country);
+                                await _context.SaveChangesAsync();
                             }
-                        },
+                        }
                     }
-                });
-                _context.Countries.Add(new Country
-                {
-                    Name = "Estados Unidos",
-                    States = new List<State>()
-                    {
-                        new State()
-                        {
-                            Name = "Florida",
-                            Cities = new List<City>() {
-                                new City() { Name = "Orlando" },
-                                new City() { Name = "Miami" },
-                                new City() { Name = "Tampa" },
-                                new City() { Name = "Fort Lauderdale" },
-                                new City() { Name = "Key West" },
-                            }
-                        },
-                        new State()
-                        {
-                            Name = "Texas",
-                            Cities = new List<City>() {
-                                new City() { Name = "Houston" },
-                                new City() { Name = "San Antonio" },
-                                new City() { Name = "Dallas" },
-                                new City() { Name = "Austin" },
-                                new City() { Name = "El Paso" },
-                            }
-                        },
-                    }
-                });
-
-                await _context.SaveChangesAsync();
-            }
-
-            if (!_context.Categories.Any())
-            {
-                _context.Categories.Add(new Category
-                {
-                    Name = "Automotores",
-                    Products = new List<Product>()
-                    {
-                        new Product()
-                        {
-                            Name = "Repuestos",
-                            prodCategories = new List<ProdCategory>() {
-                                new ProdCategory() { Name = "LLantas" },
-                                new ProdCategory() { Name = "Bacreria" },
-                                new ProdCategory() { Name = "Cogineria" },
-                                new ProdCategory() { Name = "Luces" },
-                                new ProdCategory() { Name = "Cables" },
-                            }
-                        },
-                        new Product()
-                        {
-                            Name = "Pintura",
-                            prodCategories = new List<ProdCategory>() {
-                                new ProdCategory() { Name = "Tipo 1" },
-                                new ProdCategory() { Name = "Tipo 2 A" },
-                                new ProdCategory() { Name = "Tipo 2" },
-                                new ProdCategory() { Name = "Tipo 3" },
-                                new ProdCategory() { Name = "Tipo 3 A" },
-                            }
-                        },
-                    }
-                });
-
-                _context.Categories.Add(new Category
-                {
-                    Name = "Tecnologia",
-                    Products = new List<Product>()
-                    {
-                        new Product()
-                        {
-                            Name = "Sonido",
-                            prodCategories = new List<ProdCategory>() {
-                                new ProdCategory() { Name = "Bafles" },
-                                new ProdCategory() { Name = "Bajos" },
-                                new ProdCategory() { Name = "Twuiters" },
-                                new ProdCategory() { Name = "Pantalla" },
-                                new ProdCategory() { Name = "Wopers" },
-                            }
-                        },
-                        new Product()
-                        {
-                            Name = "Electrico",
-                            prodCategories = new List<ProdCategory>() {
-                                new ProdCategory() { Name = "Vidrios 1" },
-                                new ProdCategory() { Name = "Alarma 2 A" },
-                                new ProdCategory() { Name = "Sensores 2" },
-                                new ProdCategory() { Name = "Sillas 3" },
-                                new ProdCategory() { Name = "Parabrisas 3 A" },
-                            }
-                        },
-                    }
-                });
-
-                _context.Categories.Add(new Category
-                {
-                    Name = "Casa",
-                    Products = new List<Product>()
-                    {
-                        new Product()
-                        {
-                            Name = "Remodelacion",
-                            prodCategories = new List<ProdCategory>() {
-                                new ProdCategory() { Name = "Pintura" },
-                                new ProdCategory() { Name = "Enchapes" },
-                                new ProdCategory() { Name = "Baños" },
-                                new ProdCategory() { Name = "Electricidad" },
-                                new ProdCategory() { Name = "Acueducto" },
-                            }
-                        },
-                        new Product()
-                        {
-                            Name = "Interior",
-                            prodCategories = new List<ProdCategory>() {
-                                new ProdCategory() { Name = "Juego de sala" },
-                                new ProdCategory() { Name = "Alcoba" },
-                                new ProdCategory() { Name = "Cocina" },
-                                new ProdCategory() { Name = "Patio" },
-                                new ProdCategory() { Name = "Puertas" },
-                            }
-                        },
-                    }
-                });
-
-                await _context.SaveChangesAsync();
+                }
 
 
             }
