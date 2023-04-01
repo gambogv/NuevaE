50eed47e05181308972b3631f9f57cb488d854ab
GABRIEL ANGEL VELASQUEZ RINCON <gabrielvelasquez256153@correo.itm.edu.co>
2023-04-01 17:59:44 -0500

    Confirm user register

--- Sales.API/Helpers/IUserHelper.cs
+++ Sales.API/Helpers/IUserHelper.cs
@@ -26,6 +26,11 @@ namespace Sales.API.Helpers
 
         Task<User> GetUserAsync(Guid userId);
 
+        Task<string> GenerateEmailConfirmationTokenAsync(User user);
+
+        Task<IdentityResult> ConfirmEmailAsync(User user, string token);
+
+
 
     }
 }
